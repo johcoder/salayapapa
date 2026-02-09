3b5f11e4b5ce206bc1578d7605f8486752b87957
johcoder <joramlucky90@gmail.com>
2026-02-09 17:46:30 +0300

    changepic

--- components/ui/PrayerCards.tsx
+++ components/ui/PrayerCards.tsx
@@ -52,7 +52,7 @@ export default function PrayerCards() {
         symbol={
           <div className="w-20 h-20 relative">
             <Image
-              src="/mchana2.jpg"
+              src="/mchana3.jpg"
               alt="Afternoon Sun"
               fill
               className="object-contain"
